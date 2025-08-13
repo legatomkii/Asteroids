a1087ea3eede4a60f219fa1195bd9d206da4f82b
legatomkii <christianftw@gmail.com>
2025-08-13 10:58:52 -0700

    0.0.2: Added 60 FPS limiter

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ def main():
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-   
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids")
+    pygame.display.set_caption("Asteroids 0.0.2")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+                
         screen.fill("black")
+        dt = clock.tick(60)/1000
+    
 
 if __name__ == "__main__":
     main()
