6a80c58a96bf0c7699deaedb4c6252963ccc4994
legatomkii <christianftw@gmail.com>
2025-08-14 16:09:05 -0700

    0.0.5: Added rotation logic

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids 0.0.4")
+    pygame.display.set_caption("Asteroids 0.0.5")
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         player.draw(screen) 
-        pygame.display.flip()      
+        pygame.display.flip()  
         screen.fill("black")
         dt = clock.tick(60)/1000
-        
 
 if __name__ == "__main__":
     main()
