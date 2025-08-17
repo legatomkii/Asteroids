8655fbe6677d32d0672fa623c5ebe83b94212e15
legatomkii <christianftw@gmail.com>
2025-08-17 14:51:51 -0700

    0.1.0: Added shooting functionality. Cleaned up code for readability.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import sys
 import pygame
 from constants import *
-from player import *
-from asteroidfield import *
-import sys
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
@@ -13,30 +16,48 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids 0.0.9")
+    pygame.display.set_caption("Asteroids 0.1.0")
+
+    #Creating Sprite groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+     
+    #Creating Containers
     Asteroid.containers = (asteroids, updatable, drawable)
-    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = (updatable,)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
     
+    #Creating player start position and containers
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
+    #Infinite "While" loop for gameplay
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        #Updating items in Updatables container
         for item in updatable:
             item.update(dt)
+        
+        #Detecting collision with player and asteroids, ending game if collision detected
         for item in asteroids:
             if item.collision(player) == True:
                 print("Game Over!")
                 sys.exit()
+        
+        #Drawing items in Drawables container
         for item in drawable:
             item.draw(screen)
         pygame.display.flip()  
         screen.fill("black")
+        
+        #setting Framerate at 60 FPS
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
